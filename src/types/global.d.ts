82339692547b9e9e9e589dbcf3d5cc329e59659d
AjayTeotia <teotiaajay20@gmail.com>
2025-10-03 13:33:03 +0530

    8.Daily-News-Summary

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -215,6 +215,12 @@ declare global {
     threshold: number;
     changePercent?: number;
   };
+
+  interface UserForNewsEmail {
+    id: string;
+    email: string;
+    name: string;
+  }
 }
 
 export {};
